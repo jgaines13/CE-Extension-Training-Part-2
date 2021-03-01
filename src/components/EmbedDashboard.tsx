968e5ec2fb0a6f7d36f67755cdb8634a55f07100
Jessica Gaines <jessgaines@google.com>
2021-03-01 15:28:43 -0800

    initial commit

---
+++ src/components/EmbedDashboard.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from 'react';
+import styled from "styled-components";
+import { LookerEmbedSDK } from '@looker/embed-sdk';
+
+// create iframe of dashboard
+
+
+const EmbedDashboard = (props: any) => {
+
+
+  
+  const DashboardDiv = useCallback((el:any ) => {
+    LookerEmbedSDK.init('https://marketplace.dev.looker.com')
+    LookerEmbedSDK.createDashboardWithId(props.id)
+    .appendTo(el)
+    .withNext()
+    .withFilters({'State': props.filters})
+    .build()
+    .connect()
+    .catch((error: any) => {
+      console.error('An unexpected error occurred', error)
+    })
+  }, [])
+
+ 
+
+
+  return (
+    <>
+    <Dashboard ref={DashboardDiv}></Dashboard>
+    </>
+  );
+}
+
+
+const Dashboard = styled.div`
+  width: 100%;
+  height: 95vh;
+  & > iframe {
+    width: 100%;
+    height: 100%;
+  }
+` 
+
+export default EmbedDashboard;
